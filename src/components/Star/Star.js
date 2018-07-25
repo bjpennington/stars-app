832db9cb635b34b35b4184c7b4dc90efa834eaa7
BJ Pennington <bjpennington112@gmail.com>
2018-07-25 14:01:43 -0500

    added list of stars

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -5,7 +5,10 @@ class Star extends Component {
         super(props);
 
         this.state = {
-            star : {
+            allStars : [
+
+            ],
+            newStar : {
                 name : 'Felicia Day',
                 role : 'The Guild',
                 popularity : 70
@@ -15,37 +18,23 @@ class Star extends Component {
 
     handleChangeFor = (propertyName) => (event) => {
         this.setState({
-            star : {
-                ...this.state.star,
+            newStar : {
+                ...this.state.newStar,
                 [propertyName] : event.target.value
             }
         })
     }
 
-    // handleNameChange = (event) => {
-    //     this.setState({
-    //         star : {
-    //             ...this.state.star,
-    //             name : event.target.value
-    //         }
-    //     })
-    // }
-
-    // handleRoleChange = (event) => {
-    //     this.setState({
-    //         star : {
-    //             ...this.state.star,
-    //             role : event.target.value
-    //         }
-    //     })
-    // }
-
     handleSubmit = (event) => {
-        console.log(this.state.star);
         event.preventDefault();
+        console.log(this.state);
 
         this.setState({
-            star : {
+            allStars : [
+                ...this.state.allStars,
+                this.state.newStar
+            ],
+            newStar : {
                 name : '',
                 role : '',
                 popularity : ''
@@ -55,16 +44,25 @@ class Star extends Component {
 
 
     render() {
+
+        let starList = this.state.allStars.map((star, index) => {
+            return <li key={index}>{star.name}, {star.role}, {star.popularity}</li>
+        })
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input value={this.state.star.name} type="text" placeholder="Name" onChange={this.handleChangeFor('name')} />
-                    <input value={this.state.star.role} type="text" placeholder="Role" onChange={this.handleChangeFor('role')} />
-                    <input value={this.state.star.popularity} type="number" placeholder="Popularity Rating" onChange={this.handleChangeFor('popularity')} />
+                    <input value={this.state.newStar.name} type="text" placeholder="Name" onChange={this.handleChangeFor('name')} />
+                    <input value={this.state.newStar.role} type="text" placeholder="Role" onChange={this.handleChangeFor('role')} />
+                    <input value={this.state.newStar.popularity} type="number" placeholder="Popularity Rating" onChange={this.handleChangeFor('popularity')} />
                     <input type="submit" />
                 </form>
-                <p>{this.state.star.name} is famous for {this.state.star.role}.</p>
-                <p>{this.state.star.name}'s popularity rating is {this.state.star.popularity}</p>
+                <p>{this.state.newStar.name} is famous for {this.state.newStar.role}.</p>
+                <p>{this.state.newStar.name}'s popularity rating is {this.state.newStar.popularity}</p>
+                <p>The Stars:</p>
+                <ul>
+                    {starList}
+                </ul>
             </div>
         )
     }
